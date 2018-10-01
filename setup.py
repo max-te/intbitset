e97d1569ef6b7c3a2f6e43b7a1c6531da16fc381
Max Teegen <maximilian.teegen@uni-hamburg.de>
2018-10-01 11:05:36 +0200

    Let setuptools handle cython.

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@
 """C-based extension implementing fast integer bit sets."""
 
 from setuptools import Extension, setup
-from Cython.Build import cythonize
 import os
 import re
 
@@ -46,13 +45,14 @@ setup(
     long_description=open('README.rst').read(),
     package_dir={'': 'intbitset'},
     py_modules=['intbitset_helper', 'version'],
-    ext_modules=cythonize([
+    setup_requires=['setuptools>=18.0', 'cython'],
+    ext_modules=[
         Extension("intbitset",
                   ["intbitset/intbitset.pyx", "intbitset/intbitset_impl.c"],
                   extra_compile_args=['-O3', '-march=core2', '-mtune=native']
                   # For debug -> '-ftree-vectorizer-verbose=2'
                   )
-    ]),
+    ],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
